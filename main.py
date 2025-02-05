a3ed7c246b2f471887d3a58587f73ffbc13e9b03
AnthonyRXS <arxs74@gmail.com>
2025-02-04 21:18:15 -0600

    remplazo
    reemplazo actualizacion

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-# Proyecto: [Nombre del Proyecto]
-# Estudiante: [Nombre del Estudiante]
-# Fecha de Inicio: [dd/mm/aaaa]
+# Proyecto: Proyecto #1
+# Estudiante: Anthony Rodriguez Salazar
+# Fecha de Inicio: 2025/02/04
 # Fecha de Entrega: [dd/mm/aaaa]
 # Descripción: Este archivo contiene el punto de entrada principal del proyecto.
 # Recuerda incluir tu nombre completo, la fecha en la que iniciaste el proyecto y la fecha estimada de entrega.
 # Esto ayuda a mantener un registro claro del trabajo realizado.
+
+
